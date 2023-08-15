a7d5520edf3790d13c310250fd2961586dfe39ca
user <user@ASHISH-SHARMA>
2023-08-15 10:45:34 +0530

    Add project files.

---
+++ Models/Db1Context.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace APITask.Models;
+
+public partial class Db1Context : DbContext
+{
+
+    public Db1Context(DbContextOptions<Db1Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Employee> Employees { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DB1;Integrated Security=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Employee>(entity =>
+        {
+            entity.HasKey(e => e.EmployeeId).HasName("PK__Employee__7AD04FF1596E7883");
+
+            entity.ToTable("Employee");
+
+            entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
+            entity.Property(e => e.Address)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.EmployeeDob)
+                .HasDefaultValueSql("(getdate())")
+                .HasColumnType("date")
+                .HasColumnName("EmployeeDOB");
+            entity.Property(e => e.EmployeeName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.Gender)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+            entity.Property(e => e.State)
+                .HasMaxLength(30)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC07804469A9");
+
+            entity.Property(e => e.Description)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace APITask.Models;
+
+public partial class Employee
+{
+    public int EmployeeId { get; set; }
+
+    public string EmployeeName { get; set; } = null!;
+
+    public DateTime EmployeeDob { get; set; }
+
+    public string Gender { get; set; } = null!;
+
+    public string Address { get; set; } = null!;
+
+    public string State { get; set; } = null!;
+}
